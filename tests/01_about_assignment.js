7bc6fab0ebc5cdcd61cbcf8f6f61c9de515e2e0a
Ignacio Loureiro <ignacio.loureiro@endava.com>
2020-06-29 14:17:17 -0300

    about assignment

--- tests/01_about_assignment.js
+++ tests/01_about_assignment.js
@@ -1,4 +1,4 @@
-describe("About Assignment (topics/about_assignment.js)", function () {
+fdescribe("About Assignment (topics/about_assignment.js)", function () {
   it("local variables", function () {
     let temp = 1;
     expect(temp).toBe(1, "Assign a value to the variable temp");
@@ -16,13 +16,6 @@ describe("About Assignment (topics/about_assignment.js)", function () {
     temp = 1;
   }
   it("global variables", function () {
-<<<<<<< HEAD:tests/01_about_assignment.js
-    expect(temp).toBe(1, "global variables are assigned to the window object");
-=======
-    expect(temp).toBe(
-      __,
-      "global variables are assigned to the window object"
-    );
->>>>>>> master:tests/01_about_assignment.js
+    expect(temp).toBe(1, "global variables are assigned to can be used anywhere");
   });
 });
