e9c77884e285c24ad2623280f6cd26cf93be9db8
Ignacio Loureiro <ignacio.loureiro@endava.com>
2020-06-29 12:33:06 -0300

    added some tests

--- tests/01_about_assignment.js
+++ tests/01_about_assignment.js
@@ -1,19 +1,21 @@
 describe("About Assignment (topics/about_assignment.js)", function () {
   it("local variables", function () {
-    let temp = __;
+    let temp = 1;
     expect(temp).toBe(1, "Assign a value to the variable temp");
   });
 
   it("local constant", function () {
-    const temp = __;
+    const temp = 1;
     expect(temp).toBe(1, "Assign a value to the variable temp");
   });
 
+  /* global temp */
+  /*eslint no-global-assign: "off" */
+  {
+    // this variable is declared in this scope and used outside
+    temp = 1;
+  }
   it("global variables", function () {
-    temp = 1; // Not using let is an example. Always use let/const in practise.
-    expect(window.__).toBe(
-      temp,
-      "global variables are assigned to the window object"
-    );
+    expect(temp).toBe(1, "global variables are assigned to the window object");
   });
 });
